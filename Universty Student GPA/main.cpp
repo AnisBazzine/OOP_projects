9e4366b6daad7dde38c505fbc721a60cfd057fe4
anisbazzine <mohamedanis1002@gmail.com>
2022-09-17 19:05:22 +0100

    Ah just same change

--- Universty Student GPA/main.cpp
+++ Universty Student GPA/main.cpp
@@ -3,36 +3,7 @@
 #include <limits>
 #include "Gpa.h"
 using namespace std;
-/*
-first ask 
-    Total unite you have
-    Ask in the unite about total course
-    name course
-    credit, coff, TD point, exam point 
-    for every course then calclute 
-    if the gpa less 10 
-        give you the modle you need to prepare
-        the real credit 
-    if >= 10 the credit equal 30  
------------------------------------------------------
-1 aks for how many unite do you have 
-2 for single unite ask how many courses 
-3 for single course ask about :
-        Course name 
-        Exam point 
-        Td point if not then enter exam point
-        Credit 
-        Cofffinet 
-4 if get 10 or more print  the student gpa
-    Else
-        Print gpa, cridet and  fail courses with their gpa
-*/
-/*
-I think it's a great overall program but it's missing some user experience touches:
-    1- one example for the people that don't know how to use it || Done 
-    2- shorten the time of insertion (it takes a lot of time to insert the grades) || Done
-    3- make some validation: any number or character can inserted || Done
-*/
+
 Gpa gpa;
 int totUnite = 0, courseInUnite = 0, choise;
 string courseName;
